097b8992df64c04238cd0c94918e6d90e116e3e3
Willem Larsen <willemlarsen@gmail.com>
2014-06-16 11:16:46 -0700

    pass third test, convert double digit numbers above 19

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ var convertNumbersToWords = function(number) {
 		17: "seventeen",
 		18: "eighteen",
 		19: "nineteen",
-	}
+	};
 
 	var onesPlace = {
 		1: "one",
@@ -22,14 +22,29 @@ var convertNumbersToWords = function(number) {
 		7: "seven",
 		8: "eight",
 		9: "nine"
-	}
-	
+	};
+
+	var tensPlaceTwentyAndUp = {
+		2: "twenty",
+		3: "thirty",
+		4: "forty",
+		5: "fifty",
+		6: "sixty",
+		7: "seventy",
+		8: "eighty",
+		9: "ninety"
+	};
+
 	if (number === 0) {
 		return "zero"
 	} else if (number < 10) {
 		return onesPlace[number];
 	} else if (number < 20) {
 		return tensPlaceTeens[number];
+	} else if (number < 100) {
+		var tens = Math.floor(number / 10);
+		var ones = number % 10;
+		return tensPlaceTwentyAndUp[tens] + " " + onesPlace[ones];
 	}
 		
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,7 +2,10 @@ describe('convertNumbersToWords', function () {
 	it('converts single digit numbers to their word equivalents', function() {
 		convertNumbersToWords(4).should.equal('four');
 	});
-	it("converts double-digit numbers to their word equivalents", function () {
+	it("converts double-digit numbers in the teens to their word equivalents", function () {
 		convertNumbersToWords(13).should.equal('thirteen');
+	});
+	it("converts double-digit numbers from 20 to 99 to their word equivalents", function () {
+		convertNumbersToWords(21).should.equal('twenty one');
 	});
 });
