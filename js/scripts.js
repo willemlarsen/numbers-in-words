b5cbbbe2dba1abd1381f17a89100806093b47d26
Willem Larsen <willemlarsen@gmail.com>
2014-06-16 10:48:27 -0700

    pass first test, converting single digit numbers to words

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,16 @@
-var scrabbleScore = function (word) {
-	function letterValues(letters, score) {
-		this.letters = letters;
-		this.score = score;
+var convertNumbersToWords = function(number) {
+	var onesPlace = {
+		1: "one",
+		2: "two",
+		3: "three",
+		4: "four",
+		5: "five",
+		6: "six",
+		7: "seven",
+		8: "eight",
+		9: "nine"
 	}
 
-	var valueOne = new letterValues(/[aeioulnrstAEIOULNRST]/, 1);
-
-	var valueTwo = new letterValues(/[dgDG]/, 2);
-	
-	var valueThree = new letterValues(/[bcmpBCMP]/, 3);
-	
-	var valueFour = new letterValues(/[fhvwyFHVWY]/, 4);
-
-	var valueFive = new letterValues(/[kK]/, 5);
-
-	var valueSix = new letterValues(/[jxJX]/, 6);
-
-	var valueTen = new letterValues(/[qzQZ]/, 10);
-	
-	var wordValue = 0;
-
-	var scoreArray = [valueOne, valueTwo, valueThree, valueFour, valueFive, valueSix, valueTen];
-	for (var element = 0; element < scoreArray.length; element++) {
-		for (var index = 0; index < word.length; index++) {
-			if (scoreArray[element].letters.test(word.charAt(0))) {
-				wordValue = wordValue + scoreArray[element].score;
-			}
-		} 
-	} return wordValue;
-};
-
-$(document).ready(function() {
-	$('form#scrabble-score').submit(function(event) {
-		var inputWord = $('input#word').val();
-		var calculation = scrabbleScore(inputWord);
+	return onesPlace[number];
 		
-
-		$('#input-word').text(inputWord);
-		$('#calculation').text(calculation);
-		$('.result').show();
-
-		event.preventDefault();
-	});
-});
-
-
+};
